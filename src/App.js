aedc17a8da08b3724378d0d4e98e4145a72a79d3
jm-wilson <joe.wilson@jwils.xyz>
2020-10-27 23:17:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: '#3CA9D1',
+        blue: 5,
+      },
+    },
+  },
+};
+
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signIn',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: '',
+  },
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signIn',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+      },
+    };
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch('https://pacific-earth-47843.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          fetch('https://pacific-earth-47843.herokuapp.com/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            })
+            .catch(console.log);
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === 'signOut') {
+      this.setState(initialState);
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank name={this.state.user.name} entries={this.state.user.entries} />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : this.state.route === 'register' ? (
+          <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        ) : (
+          <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,112 @@
+import React from 'react';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+    };
+  }
+
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  handleKeypress = (event) => {
+    if (event.keyCode === 13) {
+      this.onSubmitRegister();
+    }
+  };
+
+  onSubmitRegister = () => {
+    fetch('https://pacific-earth-47843.herokuapp.com/register', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange('home');
+        }
+      });
+  };
+
+  render() {
+    return (
+      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center'>
+        <main className='pa4 black-80'>
+          <div className='measure'>
+            <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+              <legend className='f1 fw6 ph0 mh0'>Register</legend>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='name'>
+                  Name
+                </label>
+                <input
+                  onChange={this.onNameChange}
+                  onKeyDown={this.handleKeypress}
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='text'
+                  name='name'
+                  id='name'
+                />
+              </div>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='email-address'>
+                  Email
+                </label>
+                <input
+                  onChange={this.onEmailChange}
+                  onKeyDown={this.handleKeypress}
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='email'
+                  name='email-address'
+                  id='email-address'
+                />
+              </div>
+              <div className='mv3'>
+                <label className='db fw6 lh-copy f6' htmlFor='password'>
+                  Password
+                </label>
+                <input
+                  onChange={this.onPasswordChange}
+                  onKeyDown={this.handleKeypress}
+                  className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                  type='password'
+                  name='password'
+                  id='password'
+                />
+              </div>
+            </fieldset>
+            <div className=''>
+              <input
+                onClick={this.onSubmitRegister}
+                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
+                type='submit'
+                value='Register'
+              />
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Register;
